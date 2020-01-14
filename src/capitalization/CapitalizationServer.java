919fa6cfceb271816154b1cb86b4c233e70538a2
sjanvey <sjanvey@169.254.125.81>
2020-01-14 14:32:27 -0500

    Capitalization Server  & Client Week 1 - IST 411

---
+++ src/capitalization/CapitalizationServer.java
@@ -0,0 +1,36 @@
+
+package capitalization;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class CapitalizationServer {
+    public static void main(String[] args) {
+        System.out.println("Capitalization Server");
+        
+        try (ServerSocket serverSocket = new ServerSocket(6000)) {
+            System.out.println("Waiting for connection......");
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Connected to client");
+            
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true); 
+                
+            String inputLine;
+            while ((inputLine = br.readLine()) != null) {
+                System.out.println("Server: " + inputLine);
+                out.println(inputLine);
+            }
+        
+        }
+        catch (IOException ex) {
+            
+        }
+        
+    }
+}
